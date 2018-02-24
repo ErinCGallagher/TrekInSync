0630e52eee2e06f8f78d9d900960c4560f05f39e
erincgallagher <erin.catherine.gallagher@gmail.com>
2018-02-24 14:17:05 -0500

    Added presenter and view interface to qr scanner activity

--- app/src/main/java/com/example/ering/trekinsync/activities/QrScannerActivity.java
+++ app/src/main/java/com/example/ering/trekinsync/activities/QrScannerActivity.java
@@ -3,45 +3,41 @@ package com.example.ering.trekinsync.activities;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.ering.trekinsync.R;
-import com.example.ering.trekinsync.models.User;
-import com.example.ering.trekinsync.utils.SharedPrefsUtils;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.example.ering.trekinsync.interfaces.QrScannerView;
+import com.example.ering.trekinsync.presenters.QrScannerPresenter;
 import com.google.zxing.Result;
-
-import java.lang.reflect.Type;
-import java.util.List;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class QrScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+public class QrScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler, QrScannerView {
 
     public static final int PERMISSION_REQUEST_CAMERA = 1;
 
     private ZXingScannerView mScannerView;
     private Context context;
+    private QrScannerPresenter presenter;
 
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
         setContentView(R.layout.activity_qr_code_scanner);
         context = getApplicationContext();
+        presenter = new QrScannerPresenter(context, this);
+
 
         ViewGroup scannerContentFrame = (ViewGroup) findViewById(R.id.scanner_frame);
         mScannerView = new ZXingScannerView(this);
         scannerContentFrame.addView(mScannerView);
 
-        if (!checkCameraPermission()) {
+        if (!presenter.isCameraAccessGranted()) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.CAMERA},
                     PERMISSION_REQUEST_CAMERA);
@@ -63,19 +59,7 @@ public class QrScannerActivity extends AppCompatActivity implements ZXingScanner
 
     @Override
     public void handleResult(Result rawResult) {
-        User userObj = convertToUserObject(rawResult.toString());
-
-        if (userObj != null && checkIfContactExists(userObj)) {
-            addContactToSharedPrefs(userObj);
-        } else {
-            Toast.makeText(this, "This travel contact already exists on your device.", Toast.LENGTH_LONG).show();
-        }
-        returnToLandingActivity();
-    }
-
-    private boolean checkCameraPermission() {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                == PackageManager.PERMISSION_GRANTED;
+        presenter.handleQrCodeScanResults(rawResult.toString());
     }
 
     @Override
@@ -94,56 +78,9 @@ public class QrScannerActivity extends AppCompatActivity implements ZXingScanner
         }
     }
 
-    private void returnToLandingActivity() {
+    @Override
+    public void returnToLandingActivity() {
         Intent intent = new Intent(context, LandingActivity.class);
         startActivity(intent);
-    }
-
-    private User convertToUserObject(String data) {
-        Gson gson = new Gson();
-        User userObj= null;
-        try {
-            userObj = gson.fromJson(data, User.class);
-        } catch(Exception e) {
-            Toast.makeText(this, "Unable to retrieve Travel Contact Details From QR code ", Toast.LENGTH_LONG).show();
-        }
-        return userObj;
-    }
-
-    /**
-     * Check if travel contact key exists in shared preferences.
-     * @param userObj, object representing user data
-     * @return true if user data exists on device, otherwise false
-     */
-    private boolean checkIfContactExists(User userObj) {
-        SharedPreferences sharedPref = context.getSharedPreferences("com.example.trekinsync.userData",Context.MODE_PRIVATE);
-        String contactJson = sharedPref.getString(SharedPrefsUtils.getKey(context, userObj), "false");
-
-        if (contactJson == "false") {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private void addContactToSharedPrefs(User userObj) {
-        SharedPreferences sharedPref = context.getSharedPreferences("com.example.trekinsync.userData",Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        Type listType = new TypeToken<List<String>>() {}.getType();
-        Gson gson = new Gson();
-
-        //retrieve list of travel contact key names in shared preferences
-        String json = sharedPref.getString(SharedPrefsUtils.getKey(context, R.string.travel_contact_key_names), "");
-        List<String> contactKeyNames = gson.fromJson(json, listType);
-
-        //Add new key name to list and put back in shared preferences
-        contactKeyNames.add(SharedPrefsUtils.createTravelContactKey(userObj));
-        String jsonKeyNames = gson.toJson(contactKeyNames);
-        editor.putString(SharedPrefsUtils.getKey(context, R.string.travel_contact_key_names), jsonKeyNames);
-
-        //Add new user details to shared preferences
-        String contactJson = gson.toJson(userObj);
-        editor.putString(SharedPrefsUtils.getKey(context, userObj), contactJson);
-        editor.apply();
     }
 }
