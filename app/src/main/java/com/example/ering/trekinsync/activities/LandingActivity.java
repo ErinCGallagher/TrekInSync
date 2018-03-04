411fb48832b682979c16a96113e2921d77fa1cf1
erincgallagher <erin.catherine.gallagher@gmail.com>
2018-03-04 12:16:20 -0500

    Move Fab meu logic to fragment

--- app/src/main/java/com/example/ering/trekinsync/activities/LandingActivity.java
+++ app/src/main/java/com/example/ering/trekinsync/activities/LandingActivity.java
@@ -6,6 +6,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -17,6 +19,7 @@ import android.widget.TextView;
 
 import com.example.ering.trekinsync.R;
 import com.example.ering.trekinsync.adapters.LandingAdapter;
+import com.example.ering.trekinsync.fragments.FabMenuFragment;
 import com.example.ering.trekinsync.interfaces.LandingView;
 import com.example.ering.trekinsync.interfaces.RecyclerViewClickListener;
 import com.example.ering.trekinsync.models.User;
@@ -31,20 +34,6 @@ public class LandingActivity extends AppCompatActivity implements RecyclerViewCl
     private RecyclerView contactListView;
     private LandingPresenter presenter;
     private LandingAdapter adapter;
-    private FloatingActionButton fabCamera;
-    private FloatingActionButton fabBarcode;
-    private TextView fabCameraLabel;
-    private TextView fabBarcodeLabel;
-    private View dimMenuBackground;
-
-    private boolean floatingButtonMenuState = false;
-    private final static double FAB_MENU_MARGIN = 0.60;
-    private final static double FAB_MENU_HEIGHT_1 = 2.15;
-    private final static double FAB_MENU_HEIGHT_2 = 3.45;
-    private final static double FAB_MENU_LABEL_HEIGHT = 1.5;
-    private final static double FAB_MENU_LABEL_MARGIN = 2.0;
-    private final static float ALPHA_FULLY_TRANSPARENT = 0.0f;
-    private final static float ALPHA_OPAQUE = 1.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +44,7 @@ public class LandingActivity extends AppCompatActivity implements RecyclerViewCl
         //create User Utils Singleton
         UserSingletonUtils.init(context);
 
+        //If on boarding has not been completed, launch intro screens
         SharedPreferences sharedPref = context.getSharedPreferences("com.example.trekinsync.userData",Context.MODE_PRIVATE);
         if (!sharedPref.getBoolean(IntroActivity.COMPLETED_ON_BOARDING, false)) {
             startActivity(new Intent(this, IntroActivity.class));
@@ -133,121 +123,16 @@ public class LandingActivity extends AppCompatActivity implements RecyclerViewCl
         startActivity(intent);
     }
 
-
-    //TODO: refactor Fab menu logic
     private void initializeFabMenu() {
-        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!floatingButtonMenuState) {
-                    displayDimBackground();
-                    displayFloatingButtonMenu();
-                    fab.setImageResource(android.R.drawable.ic_menu_close_clear_cancel);
-                } else {
-                    hideDimBackground();
-                    fab.setImageResource(R.mipmap.ic_person_add_white_24dp);
-                    hideFloatingButtonMenu();
-                }
-                floatingButtonMenuState = !floatingButtonMenuState;
-            }
-        });
-
-        fabCamera = (FloatingActionButton) findViewById(R.id.fab_camera);
-        fabCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO: close menu before leaving activity
-                //TODO: use startActivityForResult()
-                Intent intent = new Intent(context, QrScannerActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        fabBarcode = (FloatingActionButton) findViewById(R.id.fab_barcode);
-        fabBarcode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO: close menu before leaving activity
-                Intent intent = new Intent(context, DisplayQrCodeActivity.class);
-                intent.putExtra("UserObj", presenter.getUser());
-                startActivity(intent);
-            }
-        });
-
-        fabCameraLabel = (TextView) findViewById(R.id.fab_camera_label);
-        fabBarcodeLabel = (TextView) findViewById(R.id.fab_barcode_label);
-        dimMenuBackground = (View) findViewById(R.id.dim_menu_background);
-    }
-
-    private void displayDimBackground() {
-        AlphaAnimation animation1 = new AlphaAnimation(ALPHA_FULLY_TRANSPARENT, 0.65f);
-        animation1.setDuration(150);
-        dimMenuBackground.setVisibility(View.VISIBLE);
-        dimMenuBackground.startAnimation(animation1);
-    }
-
-    private void hideDimBackground() {
-        AlphaAnimation animation1 = new AlphaAnimation(0.65f, ALPHA_FULLY_TRANSPARENT);
-        animation1.setDuration(250);
-        dimMenuBackground.setVisibility(View.INVISIBLE);
-        dimMenuBackground.startAnimation(animation1);
-    }
-
-    private void displayFloatingButtonMenu() {
-        AlphaAnimation animation1 = new AlphaAnimation(ALPHA_FULLY_TRANSPARENT, ALPHA_OPAQUE);
-        animation1.setDuration(150);
-        AlphaAnimation animation2 = new AlphaAnimation(ALPHA_FULLY_TRANSPARENT, ALPHA_OPAQUE);
-        animation2.setDuration(300);
-
-        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) fabCamera.getLayoutParams();
-        layoutParams.bottomMargin = (int) (fabCamera.getHeight() * FAB_MENU_HEIGHT_1);
-        layoutParams.rightMargin = (int) (fabCamera.getWidth() * FAB_MENU_MARGIN);
-        fabCamera.setLayoutParams(layoutParams);
-        fabCamera.setVisibility(View.VISIBLE);
-        fabCamera.startAnimation(animation1);
-        fabCamera.setClickable(true);
-
-        FrameLayout.LayoutParams layoutParams2 = (FrameLayout.LayoutParams) fabBarcode.getLayoutParams();
-        layoutParams2.bottomMargin = (int) (fabBarcode.getHeight() * FAB_MENU_HEIGHT_2);
-        layoutParams2.rightMargin = (int) (fabBarcode.getWidth() * FAB_MENU_MARGIN);
-        fabBarcode.setLayoutParams(layoutParams2);
-        fabBarcode.setVisibility(View.VISIBLE);
-        fabBarcode.startAnimation(animation2);
-        fabBarcode.setClickable(true);
-
-        FrameLayout.LayoutParams layoutParamsLabel = (FrameLayout.LayoutParams) fabCameraLabel.getLayoutParams();
-        layoutParamsLabel.bottomMargin = (int) (fabCamera.getHeight() * FAB_MENU_HEIGHT_1) + (int) (fabCameraLabel.getHeight() / FAB_MENU_LABEL_HEIGHT);
-        layoutParamsLabel.rightMargin = (int) (fabCamera.getWidth() * FAB_MENU_LABEL_MARGIN);
-        fabCameraLabel.setLayoutParams(layoutParamsLabel);
-        fabCameraLabel.setVisibility(View.VISIBLE);
-        fabCameraLabel.startAnimation(animation1);
-
-        FrameLayout.LayoutParams layoutParamsLabel2 = (FrameLayout.LayoutParams) fabBarcodeLabel.getLayoutParams();
-        layoutParamsLabel2.bottomMargin = (int) (fabBarcode.getHeight() * FAB_MENU_HEIGHT_2) + (int) (fabBarcodeLabel.getHeight() / FAB_MENU_LABEL_HEIGHT);
-        layoutParamsLabel2.rightMargin = (int) (fabBarcode.getWidth() * FAB_MENU_LABEL_MARGIN);
-        fabBarcodeLabel.setLayoutParams(layoutParamsLabel2);
-        fabBarcodeLabel.setVisibility(View.VISIBLE);
-        fabBarcodeLabel.startAnimation(animation2);
-    }
-
-
-    private void hideFloatingButtonMenu() {
-        AlphaAnimation animation1 = new AlphaAnimation(ALPHA_OPAQUE, ALPHA_FULLY_TRANSPARENT);
-        animation1.setDuration(150);
-        AlphaAnimation animation2 = new AlphaAnimation(ALPHA_OPAQUE, ALPHA_FULLY_TRANSPARENT);
-        animation2.setDuration(300);
-
-        fabCamera.startAnimation(animation2);
-        fabCamera.setVisibility(View.INVISIBLE);
-        fabCamera.setClickable(false);
-        fabCameraLabel.startAnimation(animation2);
-        fabCameraLabel.setVisibility(View.INVISIBLE);
-
-        fabBarcode.startAnimation(animation1);
-        fabBarcode.setVisibility(View.INVISIBLE);
-        fabBarcode.setClickable(false);
-        fabBarcodeLabel.startAnimation(animation2);
-        fabBarcodeLabel.setVisibility(View.INVISIBLE);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FabMenuFragment fragment = new FabMenuFragment();
+        //pass in user data to fragment
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("UserObj", presenter.getUser());
+        fragment.setArguments(bundle);
+        //add fragment to activity
+        fragmentTransaction.add(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
