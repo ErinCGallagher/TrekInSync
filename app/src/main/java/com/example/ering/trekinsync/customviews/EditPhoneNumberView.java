4135c244f0180e37cab49b2b4729795d22bac37c
erincgallagher <erin.catherine.gallagher@gmail.com>
2018-03-06 21:02:01 -0500

    Update edit phone number cell to use spinners

--- app/src/main/java/com/example/ering/trekinsync/customviews/EditPhoneNumberView.java
+++ app/src/main/java/com/example/ering/trekinsync/customviews/EditPhoneNumberView.java
@@ -6,16 +6,19 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.Spinner;
 
 import com.example.ering.trekinsync.R;
+import com.example.ering.trekinsync.utils.UserSingletonUtils;
 
 public class EditPhoneNumberView extends LinearLayout {
 
-    private TextView relationshipField;
-    private TextView phoneNumberType;
+    private Spinner relationshipField;
+    private Spinner phoneNumberType;
     private EditText phoneNumber;
 
     public EditPhoneNumberView(Context context, @Nullable AttributeSet attrs) {
@@ -25,9 +28,11 @@ public class EditPhoneNumberView extends LinearLayout {
 
     private void init(final Context context, AttributeSet attrs) {
         View container = LayoutInflater.from(context).inflate(R.layout.edit_phone_number_cell, this);
-        relationshipField = (TextView) container.findViewById(R.id.relationship);
-        phoneNumberType = (TextView) container.findViewById(R.id.phone_type);
+        relationshipField = (Spinner) container.findViewById(R.id.relationship_spinner);
+        phoneNumberType = (Spinner) container.findViewById(R.id.phone_type_spinner);
         phoneNumber = (EditText) container.findViewById(R.id.edit_phone_number);
+
+        initSpinnerAdapters();
 
         if (attrs != null) {
             TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.EditPhoneNumberView, 0, 0);
@@ -41,15 +46,39 @@ public class EditPhoneNumberView extends LinearLayout {
         }
     }
 
-    public void setRelationship(String value) {
-        relationshipField.setText(value);
+    public void setRelationshipAndListener(String value, AdapterView.OnItemSelectedListener listener) {
+        setRelationship(value);
+        relationshipField.setOnItemSelectedListener(listener);
     }
 
-    public void setPhoneNumberType(String value) {
-        phoneNumberType.setText(value);
+    public void setPhoneTypeAndListener(String value, AdapterView.OnItemSelectedListener listener) {
+        setPhoneNumberType(value);
+        phoneNumberType.setOnItemSelectedListener(listener);
     }
 
     public void setPhoneNumber(String value) {
         phoneNumber.setText(value);
+    }
+
+    private void setRelationship(String value) {
+        int pos = UserSingletonUtils.getInstance().getPhoneRelationKeyPosition(value);
+        relationshipField.setSelection(pos, true);
+    }
+
+    private void setPhoneNumberType(String value) {
+        int pos = UserSingletonUtils.getInstance().getPhoneTypeKeyPosition(value);
+        phoneNumberType.setSelection(pos, true);
+    }
+
+    private void initSpinnerAdapters() {
+        ArrayAdapter<CharSequence> relationAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.contact_relation_values, android.R.layout.simple_spinner_item);
+        relationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        relationshipField.setAdapter(relationAdapter);
+
+        ArrayAdapter<CharSequence> phoneTypeAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.number_type_values, android.R.layout.simple_spinner_item);
+        phoneTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        phoneNumberType.setAdapter(phoneTypeAdapter);
     }
 }
